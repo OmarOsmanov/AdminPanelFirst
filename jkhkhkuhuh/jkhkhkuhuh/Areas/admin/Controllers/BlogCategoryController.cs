1422ed7c471bb693b3edd921d0965f695cc662c3
OmarOsmanov <tu201906019@code.edu.az>
2021-12-23 23:35:41 +0400

    first commit

---
+++ jkhkhkuhuh/jkhkhkuhuh/Areas/admin/Controllers/BlogCategoryController.cs
@@ -0,0 +1,39 @@
+﻿using jkhkhkuhuh.Data;
+using jkhkhkuhuh.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jkhkhkuhuh.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class BlogCategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BlogCategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.BlogCategories.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogCategory model)
+        {
+            _context.BlogCategories.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
